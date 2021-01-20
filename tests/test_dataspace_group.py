efb1ba1d2867d4b1cca3d1f83d12af36280aa7df
Julia Dark <julia@tiledb.com>
2021-01-20 11:23:55 -0600

    Move create_dataspace_group test into test_dataspace_group

--- tests/test_dataspace_group.py
+++ tests/test_dataspace_group.py
@@ -2,7 +2,47 @@ import numpy as np
 import pytest
 
 import tiledb
-from tiledb.cf import DataspaceGroup
+from tiledb.cf import DataspaceGroup, GroupSchema
+
+_row = tiledb.Dim(name="rows", domain=(1, 4), tile=2, dtype=np.uint64)
+_col = tiledb.Dim(name="cols", domain=(1, 8), tile=2, dtype=np.uint64)
+
+_attr_a = tiledb.Attr(name="a", dtype=np.uint64)
+_attr_b = tiledb.Attr(name="b", dtype=np.float64)
+_attr_c = tiledb.Attr(name="c", dtype=np.dtype("U"))
+_array_schema_1 = tiledb.ArraySchema(
+    domain=tiledb.Domain(_row, _col),
+    attrs=[_attr_a, _attr_b, _attr_c],
+)
+_array_schema_2 = tiledb.ArraySchema(
+    domain=tiledb.Domain(_row, _col), sparse=True, attrs=[_attr_a]
+)
+
+
+class TestCreateDataspaceGroup:
+
+    _metadata_schema = _array_schema_1
+    _array_schemas = [
+        ("A1", _array_schema_1),
+        ("A2", _array_schema_2),
+    ]
+    _group_schema = GroupSchema(_array_schemas, _metadata_schema)
+    _key = None
+
+    @pytest.fixture(scope="class")
+    def group_uri(self, tmpdir_factory):
+        """Creates a TileDB Group from GroupSchema and returns scenario dict."""
+        uri = str(tmpdir_factory.mktemp("group1"))
+        ctx = None
+        DataspaceGroup.create(uri, self._group_schema, self._key, ctx)
+        return uri
+
+    def test_array_schemas(self, group_uri):
+        uri = group_uri
+        assert tiledb.object_type(uri) == "group"
+        for name, schema in self._array_schemas:
+            array_uri = group_uri + "/" + name
+            assert tiledb.ArraySchema.load(array_uri, key=self._key) == schema
 
 
 class TestCreateMetadata:
