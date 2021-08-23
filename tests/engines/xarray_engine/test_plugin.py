ee2de3d78e418750e7f7ffc1f20c383c475b5d09
lex-c <chlochlo.lexa@gmail.com>
2021-08-23 06:01:55 -0700

    refactor test to roundtrip dataset to tdb array to dataset and add tests for values and for comparing indexing from xarray to tdb array

--- tests/engines/xarray_engine/test_plugin.py
+++ tests/engines/xarray_engine/test_plugin.py
@@ -2,211 +2,141 @@
 # Licensed under the MIT License.
 
 import numpy as np
+
+import xarray as xr
+from xarray import Variable, open_dataset
+from xarray.testing import assert_equal
+
 import pytest
+from pytest import importorskip
 
-import tiledb
+tiledb = importorskip("tiledb")
 
-xr = pytest.importorskip("xarray")
+INT_1D_DATA = np.arange(0, 16, dtype=np.int32)
+INT_1D_ATTRS = {"description": "int 1D data var"}
+INT_1D_VAR = Variable(["rows"], INT_1D_DATA, INT_1D_ATTRS)
+INT_1D_COORDS = {"rows": np.arange(0, 16, dtype=np.int32)}
 
+INT_2D_DATA = np.arange(0, 32, dtype=np.int32).reshape(8, 4)
+INT_2D_ATTRS = {"description": "int 2D data var"}
+INT_2D_VAR = Variable(["rows", "cols"], INT_2D_DATA, INT_2D_ATTRS)
+INT_2D_COORDS = {"rows": np.arange(0, 8, dtype=np.int32), "cols": np.arange(0, 4, dtype=np.int32)}
 
-class TestTileDB:
+INT_1D_SHIFTED_ATTRS = {"description": "int 1d shifted data var"}
+INT_1D_SHIFTED_VAR = Variable(["rows"], INT_1D_DATA, INT_1D_SHIFTED_ATTRS)
+INT_1D_SHIFTED_COORDS = {"rows": np.arange(5, 21, dtype=np.int32)}
 
-    FLOAT_DATA_2D = np.linspace(
-        -1.0, 1.0, num=32, endpoint=True, dtype=np.float64
-    ).reshape(8, 4)
-    INT_DATA_1D = np.arange(0, 16, dtype=np.int64)
-    INT_DATA_2D = np.arange(0, 32, dtype=np.int32).reshape(8, 4)
+INT_2D_SHIFTED_ATTRS = {"description": "int 2d shifted data var"}
+INT_2D_SHIFTED_VAR = Variable(["rows", "cols"], INT_2D_DATA, INT_2D_SHIFTED_ATTRS)
+INT_2D_SHIFTED_COORDS = {"rows": np.arange(4, 12, dtype=np.int32), "cols": np.arange(1, 5, dtype=np.int32)}
 
-    SIMPLE_DATA_ARRAYS = {
-        "simple_example_1d": xr.DataArray(
-            data=INT_DATA_1D,
-            dims="rows",
-        ),
-        "shifted_example_1d": xr.DataArray(
-            data=INT_DATA_1D,
-            dims="rows",
-            coords={"rows": np.arange(-8, 8, dtype=np.int32)},
-        ),
-        "datetime_example_1d": xr.DataArray(
-            data=INT_DATA_1D,
-            dims="rows",
-            coords={
-                "rows": np.arange(
-                    np.datetime64("2000-01-01"), np.datetime64("2000-01-17")
-                )
-            },
-        ),
-        "simple_example_2d": xr.DataArray(
-            data=INT_DATA_2D,
-            dims=["rows", "cols"],
-        ),
-        "shifted_example_2d": xr.DataArray(
-            data=FLOAT_DATA_2D,
-            dims=["rows", "cols"],
-            coords={
-                "rows": np.arange(-3, 5, dtype=np.int32),
-                "cols": np.arange(2, 6, dtype=np.int32),
-            },
-        ),
-    }
+DATETIME_1D_VAR_ATTRS = {"description": "datetime 1d data var"}
+DATETIME_1D_VAR = Variable(["rows"], INT_1D_DATA, DATETIME_1D_VAR_ATTRS)
+DATETIME_1D_COORDS = {"rows": np.arange(np.datetime64("2000-01-01"), np.datetime64("2000-01-17"), np.timedelta64(1, 'D'))}
 
-    simple_examples_1d = [
-        "simple_example_1d",
-        "shifted_example_1d",
-        "datetime_example_1d",
-    ]
+DS_ATTRS = {"description": "test dataset"}
+DATETIME_1D_DS_ATTRS = {**DS_ATTRS, "rows": {"time reference": np.datetime64("2000-01-01"), "freq": "D"}}
 
-    simple_examples_2d = ["simple_example_2d", "shifted_example_2d"]
+SAMPLE_DATASETS = {
+    "simple_1d": xr.Dataset(data_vars={"data": INT_1D_VAR}, coords=INT_1D_COORDS, attrs=DS_ATTRS),
+    "simple_2d": xr.Dataset(data_vars={"data": INT_2D_VAR}, coords=INT_2D_COORDS, attrs=DS_ATTRS),
+    "1d_shifted": xr.Dataset(data_vars={"data": INT_1D_SHIFTED_VAR}, coords=INT_1D_SHIFTED_COORDS, attrs=DS_ATTRS),
+    "2d_shifted": xr.Dataset(data_vars={"data": INT_2D_SHIFTED_VAR}, coords=INT_2D_SHIFTED_COORDS, attrs=DS_ATTRS),
+    "1d_datetime": xr.Dataset(data_vars={"data": DATETIME_1D_VAR}, coords=DATETIME_1D_COORDS, attrs=DATETIME_1D_DS_ATTRS)
+}
 
-    @pytest.fixture(scope="class")
-    def test_directory(self, tmpdir_factory):
-        return str(tmpdir_factory.mktemp("test_data"))
+class TestTDBBackend:
+    engine = "tiledb"
 
-    @pytest.fixture(scope="class")
-    def create_simple_example_data(self, test_directory):
-        # zero-based dimension
-        simple_array_uri = test_directory + "/simple_example_1d"
-        schema = tiledb.ArraySchema(
-            domain=tiledb.Domain(
-                tiledb.Dim(name="rows", domain=(0, 15), tile=4, dtype=np.int32),
-            ),
-            sparse=False,
-            attrs=[tiledb.Attr(name="data", dtype=np.int64)],
-        )
-        tiledb.DenseArray.create(simple_array_uri, schema)
-        with tiledb.DenseArray(simple_array_uri, mode="w") as array:
-            array[:] = {"data": self.INT_DATA_1D}
-        # shifted dimension (with negative values)
-        simple_array_uri = test_directory + "/shifted_example_1d"
-        schema = tiledb.ArraySchema(
-            domain=tiledb.Domain(
-                tiledb.Dim(name="rows", domain=(-8, 7), tile=4, dtype=np.int32),
-            ),
-            sparse=False,
-            attrs=[tiledb.Attr(name="data", dtype=np.int64)],
-        )
-        tiledb.DenseArray.create(simple_array_uri, schema)
-        with tiledb.DenseArray(simple_array_uri, mode="w") as array:
-            array[:] = {"data": self.INT_DATA_1D}
-        # datetime dimension
-        datetime_uri = test_directory + "/datetime_example_1d"
-        schema = tiledb.ArraySchema(
-            domain=tiledb.Domain(
-                tiledb.Dim(
-                    name="rows",
-                    domain=(
-                        np.datetime64("2000-01-01", "D"),
-                        np.datetime64("2000-01-16", "D"),
-                    ),
-                    tile=np.timedelta64(4, "D"),
-                    dtype=np.datetime64("", "D"),
-                ),
-            ),
-            attrs=[tiledb.Attr(name="data", dtype=np.int64)],
-        )
-        tiledb.DenseArray.create(datetime_uri, schema)
-        with tiledb.DenseArray(datetime_uri, mode="w") as array:
-            array[:] = {"data": self.INT_DATA_1D}
-        # simple example 2d
-        array_uri = test_directory + "/simple_example_2d"
-        schema = tiledb.ArraySchema(
-            domain=tiledb.Domain(
-                tiledb.Dim(name="rows", domain=(0, 7), tile=4, dtype=np.int32),
-                tiledb.Dim(name="cols", domain=(0, 3), tile=4, dtype=np.int32),
-            ),
-            sparse=False,
-            attrs=[tiledb.Attr(name="data", dtype=np.int32)],
-        )
-        tiledb.DenseArray.create(array_uri, schema)
-        with tiledb.DenseArray(array_uri, mode="w") as array:
-            array[:, :] = {"data": self.INT_DATA_2D}
-        # shifted example 2d
-        array_uri = test_directory + "/shifted_example_2d"
-        schema = tiledb.ArraySchema(
-            domain=tiledb.Domain(
-                tiledb.Dim(name="rows", domain=(-3, 4), tile=4, dtype=np.int32),
-                tiledb.Dim(name="cols", domain=(2, 5), tile=4, dtype=np.int32),
-            ),
-            sparse=False,
-            attrs=[tiledb.Attr(name="data", dtype=np.float64)],
-        )
-        tiledb.DenseArray.create(array_uri, schema)
-        with tiledb.DenseArray(array_uri, mode="w") as array:
-            array[:, :] = {"data": self.FLOAT_DATA_2D}
+    def open(self, path):
+        with open_dataset(path, engine=self.engine) as ds:
+            ds_obj = ds
+        return ds_obj
 
-    @pytest.fixture(scope="function")
-    def simple_data_arrays(self, test_directory, create_simple_example_data, request):
-        name = request.param
-        array_uri = test_directory + "/" + name
-        dataset = xr.open_dataset(array_uri, engine="tiledb")
-        return dataset["data"], self.SIMPLE_DATA_ARRAYS[name]
+    def to_tiledb(self, dataset, path):
+        tdb_dims = []
+        coords = dataset.coords
+        coords_attrs = dict()
+        for name in coords:
+            if name in dataset.dims:
+                if dataset.attrs.get(name) is not None:
+                    coords_attrs[name] = dataset.attrs[name]
+                coord = coords[name]
+                dtype = coord.dtype
+                if dtype.kind not in 'iuM':
+                    raise NotImplementedError(f"TDB Arrays don't work yet with this dtype coord {dtype}")
+                min_value = coord.data[0]
+                max_value = coord.data[-1]
+                if dtype.kind == "M":
+                    domain = (1, len(coord.data))
+                    dtype = np.int32
+                else:
+                    domain = (min_value + 1, max_value + 1)
+                tdb_dim = tiledb.Dim(name=name, domain=domain, dtype=dtype)
+                tdb_dims.append(tdb_dim)
+        tdb_attrs = []
+        data = dict()
+        vars_attrs = dict()
+        for var in dataset.data_vars:
+            var = dataset[var]
+            tdb_attr = tiledb.Attr(name=var.name, dtype=var.dtype)
+            tdb_attrs.append(tdb_attr)
+            data[var.name] = var.data
+            vars_attrs[var.name] = var.attrs
 
-    def test_open_multidim_dataset(self, create_tiledb_example):
-        uri, expected = create_tiledb_example
-        dataset = xr.open_dataset(uri, engine="tiledb")
-        xr.testing.assert_allclose(dataset, expected)
+        dom = tiledb.Domain(*tdb_dims)
+        schema = tiledb.ArraySchema(domain=dom, attrs=tdb_attrs, sparse=False)
+        if tiledb.array_exists(path):
+            tiledb.remove(path)
+        tiledb.DenseArray.create(path, schema)
+        with tiledb.DenseArray(path, "w") as array:
+            # only one data attr works for now
+            array[:] = list(data.values())[0]
+            for key, value in dataset.attrs.items():
+                if key not in dataset.dims and key not in dataset.data_vars:
+                    array.meta[key] = value
+            for var_name, var_attrs in vars_attrs.items():
+                for key, value in var_attrs.items():
+                    array_key = f"__tiledb_attr.{var_name}.{key}"
+                    array.meta[array_key] = value
+            for dim_name, dim_attrs in coords_attrs.items():
+                for key, value in dim_attrs.items():
+                    array_key = f"__tiledb_dim.{dim_name}.{key}"
+                    if isinstance(value, tuple) or isinstance(value, list):
+                        value = "".join(value)
+                    if isinstance(value, np.datetime64):
+                        value = str(value)
+                    array.meta[array_key] = value
+        with tiledb.DenseArray(path) as array:
+            print(array.schema, dict(array.meta))
 
-    def test_open_multidim_dataset_guess_engine(self, create_tiledb_example):
-        uri, expected = create_tiledb_example
-        dataset = xr.open_dataset(uri)
-        xr.testing.assert_allclose(dataset, expected)
+    def roundtrip(self, dataset, path):
+        self.to_tiledb(dataset, path)
+        return self.open(path)
 
-    @pytest.mark.parametrize("simple_data_arrays", simple_examples_1d, indirect=True)
-    def test_basic_indexing_1D(self, simple_data_arrays):
-        (result, expected) = simple_data_arrays
-        for index in range(expected.size):
-            xr.testing.assert_allclose(result[index], expected[index])
-            xr.testing.assert_allclose(result[-1 - index], expected[-1 - index])
-
-    @pytest.mark.parametrize("simple_data_arrays", simple_examples_1d, indirect=True)
     @pytest.mark.parametrize(
-        "index",
-        [
-            -5,
-            slice(None),
-            slice(0, 5),
-            slice(0, 5, 1),
-            slice(None, 5, 1),
-            slice(5, None, 1),
-            slice(5, 0, -2),
-            slice(1, 1),
-            np.array([0, 5, 3, 2, 1]),
-            np.array([-1, -5, -3, -2, -15]),
-            np.array([0, 0, 2, 2, 1, 3]),
-        ],
+        "ds_name, expected",
+        SAMPLE_DATASETS.items()
     )
-    def test_indexing_array_1D(self, simple_data_arrays, index):
-        (tiledb_data_array, xarray_data_array) = simple_data_arrays
-        xr.testing.assert_allclose(tiledb_data_array[index], xarray_data_array[index])
+    def test_datasets(self, ds_name, expected, tmpdir):
+        path = f"{tmpdir}/{ds_name}"
+        result = self.roundtrip(expected, path)
+        assert_equal(result, expected)
 
-    @pytest.mark.parametrize("simple_data_arrays", simple_examples_2d, indirect=True)
     @pytest.mark.parametrize(
-        "index1, index2",
-        [
-            (0, 0),
-            (1, 2),
-            (-5, 2),
-            (slice(None), slice(None)),
-            (slice(1, 8, 2), slice(0, 3)),
-            (slice(1, 1), slice(None)),
-            (slice(1, 1), 0),
-            (np.array([0, 1, 2]), 1),
-            (slice(1, 8, 2), np.array([0, 1, 2])),
-            (1, np.array([-1, -2])),
-            (np.array([0, 0, 2, 2]), np.array([1, 1, 3])),
-            (1, np.array([])),
-        ],
+        "ds_name, expected",
+        SAMPLE_DATASETS.items()
     )
-    def test_indexing_array_2D(self, simple_data_arrays, index1, index2):
-        (tiledb_data_array, xarray_data_array) = simple_data_arrays
-        xr.testing.assert_allclose(
-            tiledb_data_array[index1, index2],
-            xarray_data_array[index1, index2],
-        )
+    def test_tdb_xarray_indexing_match(self, ds_name, expected, tmpdir):
+        path = f"{tmpdir}/{ds_name}"
+        self.to_tiledb(expected, path)
+        with tiledb.DenseArray(path) as array:
+            dims = array.domain
+            min_vals = [dim.domain[0] for dim in dims]
+            for ii in np.ndindex(array.shape):
+                jj = list(ii)
+                for idx in range(len(jj)):
+                    jj[idx] += min_vals[idx]
+                jj = tuple(jj)
+                assert (array[jj]["data"] == expected.data[ii].data)
 
-    @pytest.mark.parametrize("simple_data_arrays", simple_examples_2d, indirect=True)
-    def test_indexing_array_nested_2D(self, simple_data_arrays):
-        (tiledb_data_array, xarray_data_array) = simple_data_arrays
-        result = tiledb_data_array[[0, 2, 2], [1, 3]][[0, 0, 2], 1]
-        expected = xarray_data_array[[0, 2, 2], [1, 3]][[0, 0, 2], 1]
-        xr.testing.assert_allclose(result, expected)
